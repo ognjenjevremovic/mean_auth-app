714403bcea6127bff494100a0f7ac9c9cc2af88b
Ognjen Jevremovic <ognjen.jevremovic@sbgenomics.com>
2017-03-23 22:48:17 +0100

    Basic server instance

---
+++ server.ts
@@ -0,0 +1,75 @@
+//  Dependancies
+import * as express from 'express';
+import * as morgan from 'morgan';
+import * as bodyParser from 'body-parser';
+import * as serveFavicon from 'serve-favicon';
+
+import './config';
+
+
+//  Environment variables
+const { NODE_ENV, PORT } = process.env;
+
+
+//  Initialize the express application
+const app: express.Application = express();
+
+
+/**
+ * @description
+ *  Server class, that defines
+ *  all the behaviour
+ *
+ * @class Server
+ */
+class Server {
+    app: express.Application = express();
+
+    constructor() {
+        this.registerMiddlewares();
+        this.start();
+    }
+
+    /**
+     * @description
+     *  Open the web server socket and
+     *  listen for incoming requests
+     *  on defined port
+     *
+     * @memberOf Server
+     */
+    start(): void {
+        app.listen(PORT, () => {
+            console.log(`
+                Starting. . .
+                Server listening on the port ${PORT}
+
+                If you're not redirected automaticly,
+                go to:
+                    http://localhost:${PORT}
+            `);
+        });
+    }
+
+    /**
+     * @description
+     *  Register the application middlewares
+     *
+     * @memberOf Server
+     */
+    registerMiddlewares(): void {
+        app.use(morgan(NODE_ENV));
+        app.use(bodyParser.json());
+        app.use(bodyParser.urlencoded({ extended: true }));
+
+        //  Static file server
+        // app.use('static', express.static(path.join(__dirname, 'client')));
+
+        //  Favicon serve
+        // app.use(serveFavicon(path.join(__dirname, 'public', 'favicon.png')));
+    }
+}
+
+
+//  Export the server instance
+export default new Server();
