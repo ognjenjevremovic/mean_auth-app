efbbd97f36c8c8e7a666947ac54595a04d48761c
Ognjen Jevremovic <ognjen.jevremovic@sbgenomics.com>
2017-03-23 23:11:02 +0100

    Customer Model

---
+++ models/customer.schema.ts
@@ -0,0 +1,32 @@
+//  Dependancies
+import * as mongoose from 'mongoose';
+
+//  Schema
+const { Schema } = mongoose;
+
+
+//  Customer Schema
+export const customerSchema : mongoose.SchemaDefinition = {
+    name: {
+        first: {
+            required: true,
+            type    : String,
+            trim    : true
+        },
+        last : {
+            required : true,
+            type     : String,
+            trim     : true
+        }
+    },
+    age : {
+        required : false,
+        type     : Number
+    },
+    email : {
+        required : true,
+        type     : String,
+        trim     : true,
+        unique   : true
+    }
+};
